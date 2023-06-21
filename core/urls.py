7a1d5f6b29c878f20def718d9e2602a77e83e074
MasvsaM <michu23serre@gmail.com>
2023-06-21 01:36:18 -0400

    1.0.1

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('',index, name="index"),
     path('registro/',registro, name="registro"),
-    path('/',login, name="login"),
+    path('login/',LoginView.as_view(template_name="core/admin/login.html"), name="login"),
+    path('logout/',LogoutView.as_view(template_name="core/index.html"), name="logout"),
     path('/',contactanos, name="contactanos"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
