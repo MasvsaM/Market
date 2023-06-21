4f213edaa35f65a60e2fa27553b3fcae9c1a84f5
MasvsaM <michu23serre@gmail.com>
2023-06-21 02:07:34 -0400

    1.1.2

--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Comuna(models.Model):
+    idcomuna = models.AutoField(primary_key=True)
+    descripcion= models.CharField(max_length=50)
+    estado = models.IntegerField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Genero(models.Model):
+    idgenero = models.AutoField(primary_key=True)
+    descripcion= models.CharField(max_length=50)
+    estado = models.IntegerField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    comentario = models.CharField(max_length=100)
+
+class Marca(models.Model):
+    idmarca = models.AutoField(primary_key=True)
+    descripcion= models.CharField(max_length=50)
+    estado = models.IntegerField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Celular(models.Model):
+    idcelu = models.AutoField(primary_key=True)
+    modelo = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to="img/")
+    precio = models.FloatField()
+    stock = models.IntegerField()
+    comentario = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.modelo
